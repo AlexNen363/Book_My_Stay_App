b5f572b9ddc94f381b95aa4fa9fe27c9704fb59d
AlexNen363 <abrahamcaren@gmail.com>
2026-03-24 09:12:29 +0530

    UC2 - Room types using abstraction and inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-
 public class Main {
-
     public static void main(String[] args) {
 
         System.out.println("Welcome to Book My Stay App");
-        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Hotel Booking System v1.0\n");
 
-        System.out.println("Application Started Successfully");
+        // Create room objects (Polymorphism)
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        // Static availability (simple variables)
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display details
+        r1.displayDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        r2.displayDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        r3.displayDetails();
+        System.out.println("Available: " + suiteAvailable + "\n");
     }
 }
---
+++ src/Room.java
@@ -0,0 +1,18 @@
+
+abstract class Room {
+    protected int beds;
+    protected double price;
+    protected String type;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
