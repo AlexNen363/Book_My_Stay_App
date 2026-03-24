243f01b1fc93f4c4e9a7cb2b8e15a06393de9c5f
AlexNen363 <abrahamcaren@gmail.com>
2026-03-24 09:06:31 +0530

    UC1 - Application entry and welcome message implemented

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,11 @@
-import java.util.*;
-
-class Booking {
-    int roomNumber;
-    String customerName;
-
-    Booking(int roomNumber, String customerName) {
-        this.roomNumber = roomNumber;
-        this.customerName = customerName;
-    }
-}
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Queue<Booking> queue = new LinkedList<>();
-        Set<Integer> bookedRooms = new HashSet<>();
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
 
-        // Add bookings
-        if (!bookedRooms.contains(101)) {
-            queue.add(new Booking(101, "Alice"));
-            bookedRooms.add(101);
-        }
-
-        if (!bookedRooms.contains(102)) {
-            queue.add(new Booking(102, "Bob"));
-            bookedRooms.add(102);
-        }
-
-        // Process bookings (FIFO)
-        while (!queue.isEmpty()) {
-            Booking b = queue.poll();
-            System.out.println("Room " + b.roomNumber + " booked by " + b.customerName);
-        }
+        System.out.println("Application Started Successfully");
     }
 }
